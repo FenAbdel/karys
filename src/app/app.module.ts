4e9b5da6240a0fa84e4323262814eefaedd10014
Youness El khaddar <104218650+YounessElkhaddar04@users.noreply.github.com>
2024-03-18 16:55:48 +0100

    home page + navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+// Import des modules Firebase et Angular Fire
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+// Import du module MatIconModule pour les icônes
+import { MatIconModule } from '@angular/material/icon';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HomeComponent } from './components/home/home.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp({"projectId":"karys-86c3d","appId":"1:1014848131457:web:7bb281934bf4ff464cd880","storageBucket":"karys-86c3d.appspot.com","apiKey":"AIzaSyDEjr78Xc3kVfl7YXdAj0ERd2Fc69qypYc","authDomain":"karys-86c3d.firebaseapp.com","messagingSenderId":"1014848131457"})),
+    MatIconModule, // Ajout de MatIconModule ici
+    // Initialisation de Firebase et Angular Fire
+    provideFirebaseApp(() => initializeApp({
+      "projectId": "karys-86c3d",
+      "appId": "1:1014848131457:web:7bb281934bf4ff464cd880",
+      "storageBucket": "karys-86c3d.appspot.com",
+      "apiKey": "AIzaSyDEjr78Xc3kVfl7YXdAj0ERd2Fc69qypYc",
+      "authDomain": "karys-86c3d.firebaseapp.com",
+      "messagingSenderId": "1014848131457"
+    })),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore())
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
