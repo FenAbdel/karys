2b50fafbf8ea38fffde84f4d52a84fcd29ff3a41
Kamar <kamardoblibennani@gmail.com>
2024-03-24 18:36:02 +0100

    Design001 - Compatible a la maquette

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 const routes: Routes = [
   { path: '' , redirectTo: 'home', pathMatch:'full'},
-  { path: 'home' , component: HomeComponent},
-  { path: 'navbar' , component: NavbarComponent},
-  { path: 'login', component: LoginComponent},
+  { path: 'navbar' , component: NavbarComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,16 +15,12 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { RegistrationComponent } from './registration/registration.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationComponent,
-    NavbarComponent,
-    HomeComponent,
-    LoginComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
